8a77bfb1a49e4caa9f3d353559ac3cd1f5cae1d4
Clifford Grimmell <agrimmell2@gmail.com>
2017-03-24 08:40:57 -0700

    lay down basic team structure

--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -1,3 +1,15 @@
-public class Team {
+import java.util.*;
 
+public class Team {
+  private String mTeamName;
+  private String mProjectName;
+  private String mProgramLanguage;
+  private List<String> mTeamMembers;
+
+  public Team(String teamName, String projectName, String programLanguage) {
+    mTeamName = teamName;
+    mProjectName = projectName;
+    mProgramLanguage = programLanguage;
+    mTeamMembers = new ArrayList<String>();
+  }
 }
