60a7af94d4bda356499fcbb80eacb783ae48c8b0
Clifford Grimmell <agrimmell2@gmail.com>
2017-03-24 10:04:52 -0700

    add getter methods to Team java file

--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -5,11 +5,22 @@ public class Team {
   private String mProjectName;
   private String mProgramLanguage;
   private List<String> mTeamMembers;
+  public List<Team> allTeams = new ArrayList<Team>();
 
   public Team(String teamName, String projectName, String programLanguage) {
     mTeamName = teamName;
     mProjectName = projectName;
     mProgramLanguage = programLanguage;
     mTeamMembers = new ArrayList<String>();
+    allTeams.add(this);
+  }
+  public String getTeamName() {
+    return mTeamName;
+  }
+  public String getProjectName() {
+    return mProjectName;
+  }
+  public String getProgramLanguage() {
+    return mProgramLanguage;
   }
 }
